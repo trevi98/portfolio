1228347fe71eeb9f4208e770e2a7aee5c779c244
abdullatef trefi <abdullatef.trefi98@gmail.com>
2022-08-20 20:12:49 +0300

    resume

--- index.php
+++ index.php
@@ -50,7 +50,7 @@
         <a class="text-3xl menu-links" href="#about">About</a>
         <a class="text-3xl menu-links" href="#projects">Projects</a>
         <a class="text-3xl menu-links" href="#contact">Contact</a>
-        <a class="text-3xl menu-links p-[10px] border-solid border-[1px] border-c-call-to-action-color" href="">Resume</a>
+        <a class="text-3xl menu-links p-[10px] border-solid border-[1px] border-c-call-to-action-color" href="assets/Resume.pdf" target="_blank">Resume</a>
 
     </div>
     <!-- End menu -->
@@ -89,7 +89,7 @@
                 </div>
             <div class="w-[25px] h-[25px]">
 
-                <a href="https://www.linkedin.com/in/abdullatif-treifi-9933121b7/" target="_blank">
+                <a href="https://www.linkedin.com/in/abdullatif-treifi" target="_blank">
 
                     <svg viewBox="0 0 128 128" class="fill-[#fff]">
                         <path d="M116 3H12a8.91 8.91 0 00-9 8.8v104.42a8.91 8.91 0 009 8.78h104a8.93 8.93 0 009-8.81V11.77A8.93 8.93 0 00116 3zM39.17 107H21.06V48.73h18.11zm-9-66.21a10.5 10.5 0 1110.49-10.5 10.5 10.5 0 01-10.54 10.48zM107 107H88.89V78.65c0-6.75-.12-15.44-9.41-15.44s-10.87 7.36-10.87 15V107H50.53V48.73h17.36v8h.24c2.42-4.58 8.32-9.41 17.13-9.41C103.6 47.28 107 59.35 107 75z"></path>
@@ -372,7 +372,7 @@
                     Designed & Built by Abdullatif Treifi
                 </div>
                 <div class="w-[25px] h-[25px]">
-            <a href="https://www.linkedin.com/in/abdullatif-treifi-9933121b7/" target="_blank">
+            <a href="https://www.linkedin.com/in/abdullatif-treifi" target="_blank">
 
                 <svg viewBox="0 0 128 128" class="fill-[#fff]">
                     <path d="M116 3H12a8.91 8.91 0 00-9 8.8v104.42a8.91 8.91 0 009 8.78h104a8.93 8.93 0 009-8.81V11.77A8.93 8.93 0 00116 3zM39.17 107H21.06V48.73h18.11zm-9-66.21a10.5 10.5 0 1110.49-10.5 10.5 10.5 0 01-10.54 10.48zM107 107H88.89V78.65c0-6.75-.12-15.44-9.41-15.44s-10.87 7.36-10.87 15V107H50.53V48.73h17.36v8h.24c2.42-4.58 8.32-9.41 17.13-9.41C103.6 47.28 107 59.35 107 75z"></path>
